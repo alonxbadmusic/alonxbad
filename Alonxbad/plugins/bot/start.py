45e58469d9ba2c60557ed5fea5f4393022cee3df
alonxbad <167286922+alonxbadmusic@users.noreply.github.com>
2025-07-02 00:26:07 +0500

    hmm

--- Alonxbad/plugins/bot/start.py
+++ Alonxbad/plugins/bot/start.py
@@ -1,155 +1,146 @@
 import time
-
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
-import config
+from config import START_IMG_URL, SUPPORT_GROUP, LOG_GROUP_ID, BANNED_USERS
 from Aloxbad import app
 from Aloxbad.misc import _boot_
 from Aloxbad.plugins.sudo.sudoers import sudoers_list
 from Aloxbad.utils.database import (
-    add_served_chat,
-    add_served_user,
-    blacklisted_chats,
-    get_lang,
-    is_banned_user,
-    is_on_off,
+    add_served_chat, add_served_user, blacklisted_chats,
+    get_lang, is_banned_user, is_on_off,
 )
 from Aloxbad.utils import bot_sys_stats
 from Aloxbad.utils.decorators.language import LanguageStart
 from Aloxbad.utils.formatters import get_readable_time
 from Aloxbad.utils.inline import help_pannel, private_panel, start_panel
-from config import BANNED_USERS
 from strings import get_string
 
 
-@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command("start") & filters.private & ~BANNED_USERS)
 @LanguageStart
-async def start_pm(client, message: Message, _):
+async def start_private(client, message: Message, _):
     await add_served_user(message.from_user.id)
-    if len(message.text.split()) > 1:
-        name = message.text.split(None, 1)[1]
-        if name[0:4] == "help":
-            keyboard = help_pannel(_)
+    args = message.text.split()
+
+    if len(args) > 1:
+        param = args[1]
+
+        if param.startswith("help"):
             return await message.reply_photo(
-                photo=config.START_IMG_URL,
-                caption=_["help_1"].format(config.SUPPORT_GROUP),
-                protect_content=True,
-                reply_markup=keyboard,
+                photo=START_IMG_URL,
+                caption=_["help_1"].format(SUPPORT_GROUP),
+                reply_markup=help_pannel(_),
             )
-        if name[0:3] == "sud":
+
+        elif param.startswith("sud"):
             await sudoers_list(client=client, message=message, _=_)
             if await is_on_off(2):
-                return await app.send_message(
-                    chat_id=config.LOG_GROUP_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                await app.send_message(
+                    LOG_GROUP_ID,
+                    f"{message.from_user.mention} opened sudo panel.\nID: <code>{message.from_user.id}</code>",
                 )
             return
-        if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
-            query = (str(name)).replace("info_", "", 1)
-            query = f"https://www.youtube.com/watch?v={query}"
-            results = VideosSearch(query, limit=1)
-            for result in (await results.next())["result"]:
-                title = result["title"]
-                duration = result["duration"]
-                views = result["viewCount"]["short"]
-                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-                channellink = result["channel"]["link"]
-                channel = result["channel"]["name"]
-                link = result["link"]
-                published = result["publishedTime"]
-            searched_text = _["start_6"].format(
-                title, duration, views, published, channellink, channel, app.mention
-            )
-            key = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(text=_["S_B_8"], url=link),
-                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_GROUP),
-                    ],
-                ]
-            )
-            await m.delete()
-            await app.send_photo(
-                chat_id=message.chat.id,
-                photo=thumbnail,
-                caption=searched_text,
-                reply_markup=key,
-            )
-            if await is_on_off(2):
-                return await app.send_message(
-                    chat_id=config.LOG_GROUP_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+
+        elif param.startswith("inf"):
+            m = await message.reply_text("🔎 Searching...")
+            query = param.replace("info_", "", 1)
+            results = await VideosSearch(f"https://www.youtube.com/watch?v={query}", limit=1).next()
+            for result in results["result"]:
+                searched_text = _["start_6"].format(
+                    result["title"],
+                    result["duration"],
+                    result["viewCount"]["short"],
+                    result["publishedTime"],
+                    result["channel"]["link"],
+                    result["channel"]["name"],
+                    app.mention,
                 )
-    else:
-        out = private_panel(_)
-        UP, CPU, RAM, DISK = await bot_sys_stats()
-        await message.reply_photo(
-            photo=config.START_IMG_URL,
-            caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM),
-            reply_markup=InlineKeyboardMarkup(out),
-        )
-        if await is_on_off(2):
-            return await app.send_message(
-                chat_id=config.LOG_GROUP_ID,
-                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
-            )
+                buttons = InlineKeyboardMarkup([
+                    [InlineKeyboardButton(_["S_B_8"], url=result["link"])],
+                    [InlineKeyboardButton(_["S_B_9"], url=SUPPORT_GROUP)]
+                ])
+                await m.delete()
+                return await message.reply_photo(
+                    photo=result["thumbnails"][0]["url"].split("?")[0],
+                    caption=searched_text,
+                    reply_markup=buttons
+                )
 
+            if await is_on_off(2):
+                await app.send_message(
+                    LOG_GROUP_ID,
+                    f"{message.from_user.mention} checked track info.\nID: <code>{message.from_user.id}</code>",
+                )
+            return
 
-@app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
-@LanguageStart
-async def start_gp(client, message: Message, _):
-    out = start_panel(_)
-    uptime = int(time.time() - _boot_)
+    out = private_panel(_)
+    UP, CPU, RAM, DISK = await bot_sys_stats()
     await message.reply_photo(
-        photo=config.START_IMG_URL,
-        caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
+        photo=START_IMG_URL,
+        caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM),
         reply_markup=InlineKeyboardMarkup(out),
     )
-    return await add_served_chat(message.chat.id)
+    if await is_on_off(2):
+        await app.send_message(
+            LOG_GROUP_ID,
+            f"{message.from_user.mention} started the bot.\nID: <code>{message.from_user.id}</code>",
+        )
+
+
+@app.on_message(filters.command("start") & filters.group & ~BANNED_USERS)
+@LanguageStart
+async def start_group(client, message: Message, _):
+    await add_served_chat(message.chat.id)
+    uptime = get_readable_time(int(time.time() - _boot_))
+    out = start_panel(_)
+    await message.reply_photo(
+        photo=START_IMG_URL,
+        caption=_["start_1"].format(app.mention, uptime),
+        reply_markup=InlineKeyboardMarkup(out),
+    )
 
 
 @app.on_message(filters.new_chat_members, group=-1)
-async def welcome(client, message: Message):
+async def new_member_joined(client, message: Message):
     for member in message.new_chat_members:
         try:
-            language = await get_lang(message.chat.id)
-            _ = get_string(language)
+            _ = get_string(await get_lang(message.chat.id))
+
             if await is_banned_user(member.id):
-                try:
-                    await message.chat.ban_member(member.id)
-                except:
-                    pass
+                await message.chat.ban_member(member.id)
+                continue
+
             if member.id == app.id:
                 if message.chat.type != ChatType.SUPERGROUP:
                     await message.reply_text(_["start_4"])
                     return await app.leave_chat(message.chat.id)
+
                 if message.chat.id in await blacklisted_chats():
                     await message.reply_text(
                         _["start_5"].format(
                             app.mention,
                             f"https://t.me/{app.username}?start=sudolist",
-                            config.SUPPORT_GROUP,
+                            SUPPORT_GROUP,
                         ),
                         disable_web_page_preview=True,
                     )
                     return await app.leave_chat(message.chat.id)
 
-                out = start_panel(_)
+                await add_served_chat(message.chat.id)
                 await message.reply_photo(
-                    photo=config.START_IMG_URL,
+                    photo=START_IMG_URL,
                     caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
                         message.chat.title,
                         app.mention,
                     ),
-                    reply_markup=InlineKeyboardMarkup(out),
+                    reply_markup=InlineKeyboardMarkup(start_panel(_)),
                 )
-                await add_served_chat(message.chat.id)
                 await message.stop_propagation()
-        except Exception as ex:
-            print(ex)
+
+        except Exception:
+            pass
